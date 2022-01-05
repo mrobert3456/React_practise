f5f762a25368d831331916a1c3073f12b4b288f9
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-05 14:48:59 +0100

    Added Header component

---
+++ webshop_app/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import classes from './Header.module.css';
+import mealsImg from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+          <h1>ReactMeals</h1>
+          <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+          <img src={mealsImg} alt="Table with food on it" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
