2a905d20f5d15ae8258cb52e618342942ec279e9
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-06 15:39:22 +0100

    implemented add item function in CartProvider.js

--- webshop_app/src/store/CartProvider.js
+++ webshop_app/src/store/CartProvider.js
@@ -1,18 +1,49 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
 
-const CartProvider =props=>{
-
-    const addItemToCartHandler=item=>{};
-    const removeItemFromCartHandler =id=>{};
-    const cartContext ={
-        items:[],
-        totalAmount:0,
-        addItem:addItemToCartHandler,
-        removeItem:removeItemFromCartHandler
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item); //concat returns a new array
+    //which is important, because we want to update the state in an immutable way
+    //so we will create a new state object
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
+  } else if (action.tpye === "REMOVE") {
+  }
+  return defaultCartState;
+};
 
-    return <CartContext.Provider value={cartContext}>
-        {props.children}
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (Citem) => {
+    dispatchCartAction({ type: "ADD", item: Citem });
+  };
+  const removeItemFromCartHandler = (Cid) => {
+    dispatchCartAction({ type: "REMOVE", id: Cid });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
     </CartContext.Provider>
+  );
 };
 export default CartProvider;
