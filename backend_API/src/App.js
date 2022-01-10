541174a4ad56bcfddb7f6fb314d5957c71b3d9b5
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-10 10:46:43 +0100

    Implemented http error handling in App.js

--- backend_API/src/App.js
+++ backend_API/src/App.js
@@ -1,51 +1,67 @@
-import React ,{useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies,setMovies ] =useState([]);
-  const [isLoading, setIsLoading] =useState(false);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function fetchMoviesHandler (){
+  function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
     //fetch('https://swapi.dev/api/films'); // fetch api is being used, default method is GET
     // it's an asynchron task, so we can't use the result immediately in the next line
     //it is called promise
     // IT IS LIKE ASYNC AWAIT
-    fetch('https://swapi.dev/api/films').then(response =>{
-      return response.json(); //also return a promise ->need another then()
-      //json converts the json object to a javascript object
-    }).then(data=>{
-      // START OF TRANSFORMATION
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        return response.json(); //also return a promise ->need another then()
+        //json converts the json object to a javascript object
+      })
+      .catch((errorf) => {
+        setError(errorf.message);
+        setIsLoading(false);
+      })
+      .then((data) => {
+        // START OF TRANSFORMATION
         //we need to transform the data (mapper)
-      const transformedMovies = data.results.map(movieData=>{
-        return {
-          id:movieData.episode_id,
-          title: movieData.title,
-          openingText : movieData.opening_crawl,
-          releaseData: movieData.release_date
-        };
-      });
-      //END OF TRANSFORMATION
-     setMovies(transformedMovies);
-     setIsLoading(false);
-    }); // then() -> we  can add a function, which is called when we get the data
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseData: movieData.release_date,
+          };
+        });
+        //END OF TRANSFORMATION
+        setMovies(transformedMovies);
+        setIsLoading(false);
+      }); // then() -> we  can add a function, which is called when we get the data
     //then().catch() -> catch is for error handling
-
   }
 
-
-
+    let content =<p>Found no movies</p>;
+    if(movies.length>0){
+      content = <MoviesList movies={movies}/>
+    }
+    if(error){
+      content = <p>{error}</p>
+    }
+    if(isLoading){
+      content =<p>Loading...</p>
+    }
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler} >Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        { !isLoading && movies.length<0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length ===0 && <p>Found no movies</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
