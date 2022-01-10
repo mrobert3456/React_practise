fa4ff67bd85dbfbce5fcbd12e733040f46e32581
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-10 10:13:22 +0100

    implemented fetchMovieHandler to get movies from API GET request

--- backend_API/src/App.js
+++ backend_API/src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies,setMovies ] =useState([]);
+  function fetchMoviesHandler (){
+    //fetch('https://swapi.dev/api/films'); // fetch api is being used, default method is GET
+    // it's an asynchron task, so we can't use the result immediately in the next line
+    //it is called promise
+    fetch('https://swapi.dev/api/films').then(response =>{
+      return response.json(); //also return a promise ->need another then()
+      //json converts the json object to a javascript object
+    }).then(data=>{
+      // START OF TRANSFORMATION
+        //we need to transform the data (mapper)
+      const transformedMovies = data.results.map(movieData=>{
+        return {
+          id:movieData.episode_id,
+          title: movieData.title,
+          openingText : movieData.opening_crawl,
+          releaseData: movieData.release_date
+        };
+      });
+      //END OF TRANSFORMATION
+     setMovies(transformedMovies);
+    }); // then() -> we can add a function, which is called when we get the data
+    //then().catch() -> catch is for error handling
+
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler} >Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
