9eb694bdc25a78e60aaf025c57bc52290bf535d9
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-10 10:23:09 +0100

    implemented loading handling in App.js

--- backend_API/src/App.js
+++ backend_API/src/App.js
@@ -5,10 +5,14 @@ import './App.css';
 
 function App() {
   const [movies,setMovies ] =useState([]);
+  const [isLoading, setIsLoading] =useState(false);
+
   function fetchMoviesHandler (){
+    setIsLoading(true);
     //fetch('https://swapi.dev/api/films'); // fetch api is being used, default method is GET
     // it's an asynchron task, so we can't use the result immediately in the next line
     //it is called promise
+    // IT IS LIKE ASYNC AWAIT
     fetch('https://swapi.dev/api/films').then(response =>{
       return response.json(); //also return a promise ->need another then()
       //json converts the json object to a javascript object
@@ -25,10 +29,13 @@ function App() {
       });
       //END OF TRANSFORMATION
      setMovies(transformedMovies);
-    }); // then() -> we can add a function, which is called when we get the data
+     setIsLoading(false);
+    }); // then() -> we  can add a function, which is called when we get the data
     //then().catch() -> catch is for error handling
 
   }
+
+
 
   return (
     <React.Fragment>
@@ -36,7 +43,9 @@ function App() {
         <button onClick={fetchMoviesHandler} >Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        { !isLoading && movies.length<0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
