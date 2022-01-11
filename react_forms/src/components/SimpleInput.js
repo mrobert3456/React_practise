65ca449593b769b0b7418908ce1852fef116359e
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-11 13:43:30 +0100

    implemented validation when submiting the form, with useState, useRef

--- react_forms/src/components/SimpleInput.js
+++ react_forms/src/components/SimpleInput.js
@@ -1,9 +1,60 @@
+import { useRef, useState, useEffect } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("name input is valid");
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.targer.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.prevenDefault(); // avoid sending http request to a server
+    setEnteredNameTouched(true);
+    //Validation
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    //nameInputRef.current.value=''; //it works also, but not elegant, it is better to use States then Refs in this case
+    //we directly manipulating the DOM ,so that is why is not ideal
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid =!enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must be not empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
