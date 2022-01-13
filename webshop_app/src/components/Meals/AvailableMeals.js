6f9915421ac9b4fadc14ab94553e1fc88438c63c
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-13 15:05:27 +0100

    implemented fetching meal datas with API

--- webshop_app/src/components/Meals/AvailableMeals.js
+++ webshop_app/src/components/Meals/AvailableMeals.js
@@ -1,35 +1,51 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import {useState, useCallback, useEffect} from 'react';
+
 
 const AvailableMeals = () => {
-  const mealslist = DUMMY_MEALS.map((meal) => (
+  
+  const [meals, setMeals] =useState([]);
+  const [isLoading,setIsLoading] =useState(false);
+  const [error,setError] =useState(null);
+
+  const fetchMealsHandler = useCallback(async ()=>{
+    setIsLoading(true);
+    setError(null);
+    try{
+      const response =await fetch('https://webshop-b771a-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      //default method: GET
+      //fetch returns a promise->that is why we should use async await
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json(); //getting the data ,returns an object
+      const loadedMeals =[];
+
+      for(const key in data){
+        loadedMeals.push({
+          id:key,
+          name: data[key].name,
+          description : data[key].description,
+          price : +data[key].price
+        });
+      }
+      setMeals(loadedMeals);
+    }
+    catch(error){
+      setError(error.message);
+    }
+    setIsLoading(false);
+  },[]);
+  
+  useEffect(()=>{ //should not return a promise ->1. paramn should not be an async function!!
+    fetchMealsHandler();
+  },[fetchMealsHandler]);
+
+
+  const mealslist = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -38,10 +54,21 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+
+  let content =<p>Found no datas</p>
+  if(meals.length>0){
+    content =<ul>{mealslist}</ul>;
+  }
+  if(error){
+    content =<p>{error}</p>;
+  }
+  if(isLoading){
+    content = <p>Loading....</p>
+  }
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealslist}</ul>
+        {content}
       </Card>
     </section>
   );
