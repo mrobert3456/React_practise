8781498df3ae5d368c02e794f5286052edad7ee2
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-17 11:19:19 +0100

    using redux data in the Counter.js component

--- react_redux/src/components/Counter.js
+++ react_redux/src/components/Counter.js
@@ -1,12 +1,16 @@
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  //useSelector can retrieve one slice of state property
+  // and it also subscribe to the store automatically, so this component will recieve the latest state
+  //whenever the store changes
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
