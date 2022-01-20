ba27bfd4964baa47c74dd915f74aad79e50d9815
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-20 10:45:31 +0100

    implemented automatic navigation when adding a new quote

--- Router_practise/src/pages/NewQuote.js
+++ Router_practise/src/pages/NewQuote.js
@@ -1,7 +1,13 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+
 const NewQuote = () => {
+  const history =useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes"); //navigate away if we sent the data
+
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
