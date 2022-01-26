505b3191cf3c5a4835b8a5db82c468a3bceb0cb6
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-26 10:45:57 +0100

    added a meetuplist

--- nextjs_meetup/pages/index.js
+++ nextjs_meetup/pages/index.js
@@ -1,25 +1,26 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id : 'm1',
-        title : 'First meetup',
-        image : 'https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg',
-        address : 'Some Address 5, 12345 Some city',
-        description : 'This is a the first meetup'
-    },
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg",
+    address: "Some Address 5, 12345 Some city",
+    description: "This is a the first meetup",
+  },
 
-    {
-        id : 'm2',
-        title : 'Second meetup',
-        image : 'https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg',
-        address : 'Some Address 5, 12345 Some city',
-        description : 'This is a the second meetup'
-    }
-]
-function HomePage (){
-
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+  {
+    id: "m2",
+    title: "Second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg",
+    address: "Some Address 5, 12345 Some city",
+    description: "This is a the second meetup",
+  },
+];
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
