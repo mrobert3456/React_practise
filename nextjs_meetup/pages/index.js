65ffe4c2ad210ecf29fe56560a7bf44ff8f62fa6
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-26 10:21:30 +0100

    added a new project for nextjs

---
+++ nextjs_meetup/pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id : 'm1',
+        title : 'First meetup',
+        image : 'https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg',
+        address : 'Some Address 5, 12345 Some city',
+        description : 'This is a the first meetup'
+    },
+
+    {
+        id : 'm2',
+        title : 'Second meetup',
+        image : 'https://upload.wikimedia.org/wikipedia/commons/6/6b/ThePantheon.jpg',
+        address : 'Some Address 5, 12345 Some city',
+        description : 'This is a the second meetup'
+    }
+]
+function HomePage (){
+
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage;
