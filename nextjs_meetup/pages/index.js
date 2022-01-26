d08efd6c11044d674cef4f954dbf4f664929f917
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-26 12:11:19 +0100

    implemented server side rendering

--- nextjs_meetup/pages/index.js
+++ nextjs_meetup/pages/index.js
@@ -28,18 +28,34 @@ function HomePage(props) {
   //   }, []);
   return <MeetupList meetups={props.meetups} />;
 }
-export async function getStaticProps() {
-  // this will be executed before the component function
-  //prepares props for the component
-  //so the component will be rendered with the required data
-  //so the data will appear in the html code,which is good for SEO
+// export async function getStaticProps() {
+//   // this will be executed before the component function
+//   //prepares props for the component
+//   //so the component will be rendered with the required data
+//   //so the data will appear in the html code,which is good for SEO
 
-  //fetch data from an API
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
+//   //fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 10, //incremental static generation
+//     //this would be regenerated on the server at least every 10 esconds, if there are requests coming in
+//     // the data is never older then 10 seconds
+//     //so you dont have to rebuild and redeploy the entire app ,when some date changed
+// };
+// }
+
+export async function getServerSideProps (context){
+    const req =context.req;
+    const res = context.res;
+
+    //run in the server after deployment
+    //fetch data..
+    return {
+        props:{
+            meetups: DUMMY_MEETUPS
+        }
+    };
 }
-
 export default HomePage;
