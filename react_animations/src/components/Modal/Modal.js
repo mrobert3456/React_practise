bb43711d7d287c7be90d3991a462109c2f8cd16e
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-27 13:16:31 +0100

    implemented react Transition for animating the Modal component

--- react_animations/src/components/Modal/Modal.js
+++ react_animations/src/components/Modal/Modal.js
@@ -1,17 +1,44 @@
 import React from "react";
-
+import Transition from "react-transition-group/cjs/Transition";
 import "./Modal.css";
 
+
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+  //   const cssClasses = [
+  //     "Modal",
+  //     // props.show === "entering"
+  //     //   ? "ModalOpen"
+  //     //   : props.show === "exiting"
+  //     //   ? "ModalClosed"
+  //     //   : null,
+  //   ];
 
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
