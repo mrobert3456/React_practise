5721c7c618e5316cc59c8309406abc706032e81a
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-28 11:04:26 +0100

    optimized the custom hook to avoid unnecessary component re-rendering

--- replace_redux/src/components/Products/ProductItem.js
+++ replace_redux/src/components/Products/ProductItem.js
@@ -6,11 +6,11 @@ import "./ProductItem.css";
 import { useStore } from "../../hooks-store/store";
 //import { toggleFav } from '../../store/actions/products';
 //import { ProductsContext } from "../../context/products-context";
-const ProductItem = (props) => {
+const ProductItem = React.memo((props) => {
   // const dispatch = useDispatch();
   //const toggleFav = useContext(ProductsContext).toggleFav;
 
-  const dispatch = useStore()[1];
+  const dispatch = useStore(false)[1];
   const toggleFavHandler = () => {
     //  dispatch(toggleFav(props.id));
     //toggleFav(props.id);
@@ -31,6 +31,6 @@ const ProductItem = (props) => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- replace_redux/src/hooks-store/store.js
+++ replace_redux/src/hooks-store/store.js
@@ -5,11 +5,15 @@ let listeners = [];
 
 let actions = {};
 
-export const useStore = () => {
+//shouldListen means that, only listen for changes when state changes
+//not listen when dispatching actions
+    //this would mean that only re-render components, which state is being updated
+    //with this we avoid unnecesarry re-rendering
+export const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1];
 
   const dispatch = (actionIdentifier, payload) => {
-    const newState = actions[actionIdentifier](globalState,payload);
+    const newState = actions[actionIdentifier](globalState, payload);
     globalState = { ...globalState, ...newState };
 
     for (const listener of listeners) {
@@ -19,20 +23,24 @@ export const useStore = () => {
   };
 
   useEffect(() => {
-    listeners.push(setState); //every component, which uses this custom hook will get its own setState function
-
+    if (shouldListen) {
+      listeners.push(setState); //every component, which uses this custom hook will get its own setState function
+    }
     return () => {
       //cleanup function, which will run when the component unmounts
       //we want to remove the corresponding setState function from the listeners array
-      listeners = listeners.filter((li) => li != setState);
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li != setState);
+      }
     };
-  }, []); // we could add the setState as a dependency, but react ensures that this is never changes
+  }, [shouldListen]); // we could add the setState as a dependency, but react ensures that this is never changes
   //as its coming from useState
 
   return [globalState, dispatch]; // returns a state, and a dispatch function like useReducer
 };
 
-export const initStore = (useActions, initialState) => { //for setting actions ,and state
+export const initStore = (useActions, initialState) => {
+  //for setting actions ,and state
   if (initialState) {
     globalState = { ...globalState, ...initialState };
   }
