a8f91134dc3418fa0377f0228e5beddea7897bb0
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-28 15:02:47 +0100

    grouping test

--- unit_test/src/components/Greeting.test.js
+++ unit_test/src/components/Greeting.test.js
@@ -1,13 +1,16 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
-test("renders hello word as a text", () => {
-  //arrange
-  render(<Greeting />);
 
-  //Act
-  //...nothing here
+describe("Greeting component", () => {
+  test("renders hello word as a text", () => {
+    //arrange
+    render(<Greeting />);
 
-  //assert
-  const helloWordElement = screen.getByText(/hello word/i);
-  expect(helloWordElement).toBeInTheDocument();
+    //Act
+    //...nothing here
+
+    //assert
+    const helloWordElement = screen.getByText(/hello word/i);
+    expect(helloWordElement).toBeInTheDocument();
+  });
 });
