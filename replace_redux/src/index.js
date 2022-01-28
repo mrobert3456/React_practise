342c0ba8ee4111df65c76a82253e70827a77acbb
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-28 09:25:11 +0100

    added a new project

---
+++ replace_redux/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import ProductsProvider from "./context/products-context";
+import "./index.css";
+import App from "./App";
+//import productReducer from './store/reducers/products';
+
+// const rootReducer = combineReducers({
+//   shop: productReducer
+// });
+
+// const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById("root")
+);
