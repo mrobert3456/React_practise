4dce0fd06cdd1c3695687bacef5bdb8a7d550eb3
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-31 11:09:14 +0100

    created mock for the fetch method

--- unit_test/src/components/Async.test.js
+++ unit_test/src/components/Async.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 describe('Async component', ()=>{
     test('renders posts if request succeeds', async()=>{
+        //mocking
+        //overrideing the built in fetch with our dummy fetch
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () =>[{id: 'p1', title: 'first post'}]
+        });
         render(<Async/>);
 
        const listItemElements = await screen.findAllByRole('listitem'); //find returns a promise
