f33c6201746a815ea43f5d0e0ce072bbc786545b
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-02-01 15:21:35 +0100

    added a from to add new todos

--- react_with_typescript/react-ts/src/App.tsx
+++ react_with_typescript/react-ts/src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 function App() {
-  const todos =[
-    new Todo("learn react"),
-    new Todo("learn typescript")
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
