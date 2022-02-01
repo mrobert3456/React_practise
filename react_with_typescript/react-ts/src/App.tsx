1058a5b94a309fa26c3fc81d187f8a3422ee45c0
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-02-01 13:49:23 +0100

    displayed list with todos

--- react_with_typescript/react-ts/src/App.tsx
+++ react_with_typescript/react-ts/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 function App() {
+  const todos =[
+    new Todo("learn react"),
+    new Todo("learn typescript")
+  ];
   return (
     <div>
-      
+      <Todos items={todos}/>
     </div>
   );
 }
